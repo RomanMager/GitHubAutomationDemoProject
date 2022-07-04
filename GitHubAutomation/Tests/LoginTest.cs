48766972378114b42c1333d2604ab718934e2f9a
Roman Mager <roman.mager@outlook.com>
2022-07-04 11:57:46 +0300

    Make email & password constants fields

--- GitHubAutomation/Tests/LoginTest.cs
+++ GitHubAutomation/Tests/LoginTest.cs
@@ -4,13 +4,14 @@ namespace GitHubAutomation.Tests;
 
 public class LoginTest : BaseTest
 {
+    private const string Email = "";
+    private const string Password = "";
+
     [Test]
     public void UserCanLogIn_WithValidCredentials()
     {
         HomePage.ClickOnLoginButton();
-        string email = "secex50043@shbiso.com";
-        string password = "Inno@10021998";
-        LoginPage.LogInWithValidCredentials(email, password);
+        LoginPage.LogInWithValidCredentials(Email, Password);
         Assert.AreEqual(MainPage.GetWelcomeMessageText(), "The home for all developers — including you.");
     }
 }
